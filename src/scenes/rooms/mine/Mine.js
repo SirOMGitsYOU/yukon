49b2f769081059104ed94db647fb42711af8f1b4
Peter <omgpieisawesome@hotmail.com>
2023-11-18 01:58:46 +0000

    update

--- src/scenes/rooms/mine/Mine.js
+++ src/scenes/rooms/mine/Mine.js
@@ -17,40 +17,40 @@ class Mine extends Phaser.Scene {
 	editorCreate() {
 
 		// mine_background_png
-		this.add.image(0, 0, "mine", "mine-background.png");
+		this.add.image(762, 487, "mine", "mine-background.png");
 
 		// goldminemine_goldmine
-		const goldminemine_goldmine = this.add.sprite(5, -251, "mine", "mine-goldmine0001.png");
+		const goldminemine_goldmine = this.add.sprite(767, 236, "mine", "mine-goldmine0001.png");
 		goldminemine_goldmine.play("goldminemine-goldmine");
 
 		// mine_lights_png
-		this.add.image(39, -321, "mine", "mine-lights.png");
+		this.add.image(801, 166, "mine", "mine-lights.png");
 
 		// mine_prgoldmineandtracks_png
-		this.add.image(134, -197, "mine", "mine-prgoldmineandtracks.png");
+		this.add.image(896, 290, "mine", "mine-prgoldmineandtracks.png");
 
 		// mine_cartsandrocks_png
-		this.add.image(176, -157, "mine", "mine-cartsandrocks.png");
+		this.add.image(938, 330, "mine", "mine-cartsandrocks.png");
 
 		// mine_prtable_png
-		this.add.image(-174, -135, "mine", "mine-prtable.png");
+		this.add.image(588, 352, "mine", "mine-prtable.png");
 
 		// mine_foreground_png
-		this.add.image(3, -9, "mine", "mine-foreground.png");
+		this.add.image(765, 478, "mine", "mine-foreground.png");
 
 		// phonemine_prphone
-		const phonemine_prphone = this.add.sprite(-223, -137, "mine", "mine-prphone0001.png");
+		const phonemine_prphone = this.add.sprite(539, 350, "mine", "mine-prphone0001.png");
 		phonemine_prphone.play("phonemine-prphone");
 
 		// bulbmine_bulb
-		const bulbmine_bulb = this.add.sprite(63, -305, "mine", "mine-bulb0001.png");
+		const bulbmine_bulb = this.add.sprite(825, 182, "mine", "mine-bulb0001.png");
 		bulbmine_bulb.play("bulbmine-bulb");
 
 		// mine_puffleshadow_png
-		this.add.image(-119, -130, "mine", "mine-puffleshadow.png");
+		this.add.image(643, 357, "mine", "mine-puffleshadow.png");
 
 		// pufflemine_puffle
-		const pufflemine_puffle = this.add.sprite(-119, -146, "mine", "mine-puffle0001.png");
+		const pufflemine_puffle = this.add.sprite(643, 341, "mine", "mine-puffle0001.png");
 		pufflemine_puffle.play("pufflemine-puffle");
 
 		this.events.emit("scene-awake");
